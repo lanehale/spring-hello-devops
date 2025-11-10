71b284022683456264d7896eb18861153a6ac030
lanehale <lanehale1@gmail.com>
2025-11-09 19:15:21 -0700

    Add health checks

--- src/main/java/com/example/webapp/Application.java
+++ src/main/java/com/example/webapp/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootApplication
 @RestController
@@ -12,6 +13,11 @@ public class Application {
     @GetMapping("/")
     public String sayHello() {
         return "Hello from Spring Boot on Kubernetes!";
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
     }
 
     public static void main(String[] args) {
